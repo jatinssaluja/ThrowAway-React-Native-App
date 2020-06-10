95cda0b0eee516a0003a1a9e790b445be9b485a8
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-06-10 21:55:05 +0200

    Create FormImagePicker component to enable users select images in the form

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -6,16 +6,28 @@ import {
   Form,
   FormField,
   FormPicker as Picker,
-  SubmitButton,
+  SubmitButton
 } from "../components/forms";
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import Screen from "../components/Screen";
+import FormImagePicker from "../components/forms/FormImagePicker";
 
 const validationSchema = Yup.object().shape({
-  title: Yup.string().required().min(1).label("Title"),
-  price: Yup.number().required().min(1).max(10000).label("Price"),
+  title: Yup.string()
+    .required()
+    .min(1)
+    .label("Title"),
+  price: Yup.number()
+    .required()
+    .min(1)
+    .max(10000)
+    .label("Price"),
   description: Yup.string().label("Description"),
-  category: Yup.object().required().nullable().label("Category"),
+  category: Yup.object()
+    .required()
+    .nullable()
+    .label("Category"),
+  images: Yup.array().min(1, "Please select at least one image.")
 });
 
 const categories = [
@@ -23,56 +35,56 @@ const categories = [
     backgroundColor: "#fc5c65",
     icon: "floor-lamp",
     label: "Furniture",
-    value: 1,
+    value: 1
   },
   {
     backgroundColor: "#fd9644",
     icon: "car",
     label: "Cars",
-    value: 2,
+    value: 2
   },
   {
     backgroundColor: "#fed330",
     icon: "camera",
     label: "Cameras",
-    value: 3,
+    value: 3
   },
   {
     backgroundColor: "#26de81",
     icon: "cards",
     label: "Games",
-    value: 4,
+    value: 4
   },
   {
     backgroundColor: "#2bcbba",
     icon: "shoe-heel",
     label: "Clothing",
-    value: 5,
+    value: 5
   },
   {
     backgroundColor: "#45aaf2",
     icon: "basketball",
     label: "Sports",
-    value: 6,
+    value: 6
   },
   {
     backgroundColor: "#4b7bec",
     icon: "headphones",
     label: "Movies & Music",
-    value: 7,
+    value: 7
   },
   {
     backgroundColor: "#a55eea",
     icon: "book-open-variant",
     label: "Books",
-    value: 8,
+    value: 8
   },
   {
     backgroundColor: "#778ca3",
     icon: "application",
     label: "Other",
-    value: 9,
-  },
+    value: 9
+  }
 ];
 
 function ListingEditScreen() {
@@ -84,10 +96,12 @@ function ListingEditScreen() {
           price: "",
           description: "",
           category: null,
+          images: []
         }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={values => console.log(values)}
         validationSchema={validationSchema}
       >
+        <FormImagePicker name="images" />
         <FormField maxLength={255} name="title" placeholder="Title" />
         <FormField
           keyboardType="numeric"
@@ -119,7 +133,7 @@ function ListingEditScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
-  },
+    padding: 10
+  }
 });
 export default ListingEditScreen;
