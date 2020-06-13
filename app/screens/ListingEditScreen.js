831355646c39172d6260b36e558b28cd4ec11395
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-06-13 20:08:04 +0200

    Add api layer to enable communication with backend

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
@@ -6,29 +6,21 @@ import {
   Form,
   FormField,
   FormPicker as Picker,
-  SubmitButton
+  SubmitButton,
 } from "../components/forms";
 import CategoryPickerItem from "../components/CategoryPickerItem";
 import Screen from "../components/Screen";
 import FormImagePicker from "../components/forms/FormImagePicker";
+import listingsApi from "../api/listings";
 import useLocation from "../hooks/useLocation";
+import UploadScreen from "./UploadScreen";
 
 const validationSchema = Yup.object().shape({
-  title: Yup.string()
-    .required()
-    .min(1)
-    .label("Title"),
-  price: Yup.number()
-    .required()
-    .min(1)
-    .max(10000)
-    .label("Price"),
+  title: Yup.string().required().min(1).label("Title"),
+  price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
-  category: Yup.object()
-    .required()
-    .nullable()
-    .label("Category"),
-  images: Yup.array().min(1, "Please select at least one image.")
+  category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "Please select at least one image."),
 });
 
 const categories = [
@@ -36,71 +28,95 @@ const categories = [
     backgroundColor: "#fc5c65",
     icon: "floor-lamp",
     label: "Furniture",
-    value: 1
+    value: 1,
   },
   {
     backgroundColor: "#fd9644",
     icon: "car",
     label: "Cars",
-    value: 2
+    value: 2,
   },
   {
     backgroundColor: "#fed330",
     icon: "camera",
     label: "Cameras",
-    value: 3
+    value: 3,
   },
   {
     backgroundColor: "#26de81",
     icon: "cards",
     label: "Games",
-    value: 4
+    value: 4,
   },
   {
     backgroundColor: "#2bcbba",
     icon: "shoe-heel",
     label: "Clothing",
-    value: 5
+    value: 5,
   },
   {
     backgroundColor: "#45aaf2",
     icon: "basketball",
     label: "Sports",
-    value: 6
+    value: 6,
   },
   {
     backgroundColor: "#4b7bec",
     icon: "headphones",
     label: "Movies & Music",
-    value: 7
+    value: 7,
   },
   {
     backgroundColor: "#a55eea",
     icon: "book-open-variant",
     label: "Books",
-    value: 8
+    value: 8,
   },
   {
     backgroundColor: "#778ca3",
     icon: "application",
     label: "Other",
-    value: 9
-  }
+    value: 9,
+  },
 ];
 
 function ListingEditScreen() {
   const location = useLocation();
+  const [uploadVisible, setUploadVisible] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  const handleSubmit = async (listing, { resetForm }) => {
+    setProgress(0);
+    setUploadVisible(true);
+    const result = await listingsApi.addListing(
+      { ...listing, location },
+      (progress) => setProgress(progress)
+    );
+
+    if (!result.ok) {
+      setUploadVisible(false);
+      return alert("Could not save the listing");
+    }
+
+    resetForm();
+  };
+
   return (
     <Screen style={styles.container}>
+      <UploadScreen
+        onDone={() => setUploadVisible(false)}
+        progress={progress}
+        visible={uploadVisible}
+      />
       <Form
         initialValues={{
           title: "",
           price: "",
           description: "",
           category: null,
-          images: []
+          images: [],
         }}
-        onSubmit={values => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
         <FormImagePicker name="images" />
@@ -135,7 +151,7 @@ function ListingEditScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10
-  }
+    padding: 10,
+  },
 });
 export default ListingEditScreen;
