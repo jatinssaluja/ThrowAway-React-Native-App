c3dece030c543be81f79391f406cb698a069a0b3
Jatin Singh Saluja <jatinsinghsaluja@gmail.com>
2020-06-14 22:55:31 +0200

    Add caching layer support using AsyncStorage

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Image } from "react-native-expo-image-cache";
 
 import colors from "../config/colors";
 import ListItem from "../components/lists/ListItem";
@@ -10,7 +11,12 @@ function ListingDetailsScreen({ route }) {
 
   return (
     <View>
-      <Image style={styles.image} source={listing.image} />
+      <Image
+        style={styles.image}
+        preview={{ uri: listing.images[0].thumbnailUrl }}
+        tint="light"
+        uri={listing.images[0].url}
+      />
       <View style={styles.detailsContainer}>
         <Text style={styles.title}>{listing.title}</Text>
         <Text style={styles.price}>${listing.price}</Text>
@@ -28,25 +34,25 @@ function ListingDetailsScreen({ route }) {
 
 const styles = StyleSheet.create({
   detailsContainer: {
-    padding: 20
+    padding: 20,
   },
   image: {
     width: "100%",
-    height: 300
+    height: 300,
   },
   price: {
     color: colors.secondary,
     fontWeight: "bold",
     fontSize: 20,
-    marginVertical: 10
+    marginVertical: 10,
   },
   title: {
     fontSize: 24,
-    fontWeight: "500"
+    fontWeight: "500",
   },
   userContainer: {
-    marginVertical: 40
-  }
+    marginVertical: 40,
+  },
 });
 
 export default ListingDetailsScreen;
